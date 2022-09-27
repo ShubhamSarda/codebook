079e4dd207fedf2eeac34df27cb3f974c11c2c08
Shubham Sarda <shubhamsarda99@yahoo.com>
2022-09-28 02:04:39 +0530

    Login As Guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,6 +23,21 @@ export const Login = () => {
       toast.error(error.message, {closeButton: true, position: "bottom-center"});
     }
   }
+  
+  async function handleLoginGuest(){
+    email.current.value = "shubham@example.com";
+    password.current.value = "learnreact";
+    try{
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value
+      }
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch(error){
+      toast.error(error.message, {closeButton: true, position: "bottom-center"});
+    }
+  }
 
   return (
     <main>
@@ -40,7 +55,7 @@ export const Login = () => {
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
         </form>
-        {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+        <button onClick={handleLoginGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
   )
 }
