10c6615c0d353d19b8038f23f201b330f6f55441
Shubham Sarda <shubhamsarda99@yahoo.com>
2022-09-28 01:31:52 +0530

    Updated Toastify

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import { useTitle } from "../hooks/useTitle";
 import { Rating } from "../components";
 import { useCart } from "../context";
 import { getProduct } from "../services";
-import { toast } from "react-toastify";
 
 export const ProductDetail = () => {
   const { cartList, addToCart, removeFromCart } = useCart();
