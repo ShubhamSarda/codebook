2f99b1f685c03005a0cfd6f5d41765293e3a49db
Shubham Sarda <shubhamsarda99@yahoo.com>
2022-09-28 00:29:31 +0530

    CodeBook Deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { AllRoutes } from './routes/AllRoutes';
+import { Footer, Header } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App dark:bg-dark">
+      <Header />
+      <AllRoutes />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { FilterProvider, CartProvider } from "./context";
+import { ScrollToTop } from "./components";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <CartProvider>
+        <FilterProvider>
+          <ScrollToTop />
+          <ToastContainer closeButton={false} autoClose={3000} position={"bottom-right"} />
+          <App />
+        </FilterProvider>
+      </CartProvider>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
